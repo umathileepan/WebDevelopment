cba89d356d99bb0b34287fd9540d2d8e25522b20
umathileepan <thileepan_uma@yahoo.co.uk>
2025-05-12 10:53:47 +0100

    Number Game function

--- DOM/script.js
+++ DOM/script.js
@@ -1,1 +1,17 @@
-document.querySelector("h1").innerHTML="Good Bye";
+var num = [];
+var count = 1;
+
+function fizzBuzz(){
+    num.push(count);
+    
+    if(count % 3 === 0 && count % 5 == 0){
+        num.push("FizzBuzz");
+    }else if(count % 3 === 0){
+        num.push("Fizz");
+    }else if(count % 5 == 0){
+        num.push("Buzz");
+    }
+    count++;
+    console.log(num);
+}
+ 
