64d6ea1e89da37070d429e45a9cc52d1efb10271
umathileepan <thileepan_uma@yahoo.co.uk>
2025-05-14 20:18:12 +0100

    Practise finished.

--- DOM/script.js
+++ DOM/script.js
@@ -1,17 +1,3 @@
-var num = [];
-var count = 1;
 
-function fizzBuzz(){
-    num.push(count);
-    
-    if(count % 3 === 0 && count % 5 == 0){
-        num.push("FizzBuzz");
-    }else if(count % 3 === 0){
-        num.push("Fizz");
-    }else if(count % 5 == 0){
-        num.push("Buzz");
-    }
-    count++;
-    console.log(num);
-}
+
  
