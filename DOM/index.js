c33bb8d25000d19b320b1f1087874ad4b29d5f1c
umathileepan <thileepan_uma@yahoo.co.uk>
2025-05-15 22:23:41 +0100

    Random number for Player 2 and the winner has been sorted.

--- DOM/index.js
+++ DOM/index.js
@@ -6,3 +6,21 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src",imgpath);
 
+var imgPlayer2 = Math.floor(Math.random()*6)+1;
+
+var imgpath2 = "images/dice" + imgPlayer2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",imgpath2);
+
+if(imgPlayer1 > imgPlayer2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins"; 
+}else if(imgPlayer2 > imgPlayer1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+}else{
+    document.querySelector("h1").innerHTML = "Refresh Me";
+}
+
+
+
