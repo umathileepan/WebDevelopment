5f5d1ee18d66d7ed36c1fbfb280e2ef488108f96
umathileepan <thileepan_uma@yahoo.co.uk>
2025-05-19 19:38:54 +0100

    Added EventListener for each button

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,9 @@
-document.querySelector("button").addEventListener("click",handleClick);
+var numberOfButtons = document.querySelectorAll(".drum").length;
 
-function handleClick(){
+for(var i = 0; i<numberOfButtons; i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function (){
     alert("I got click");
+}
+);
 }
 
