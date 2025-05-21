5dd289dfebdbb64d74fbee169bb350498c710e05
umathileepan <thileepan_uma@yahoo.co.uk>
2025-05-21 20:10:10 +0100

    Changed the colour of text when click

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,7 +2,10 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for(var i = 0; i<numberOfButtons; i++){
 document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-    alert("I got click");
+    var audio = new Audio("./sounds/tom-1.mp3");
+    audio.play();
+    console.log(this);
+    this.style="color:white";
 }
 );
 }
