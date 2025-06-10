b1b294dc31f188e14f5bcfaac31270d6afa1cbb4
umathileepan <thileepan_uma@yahoo.co.uk>
2025-06-10 15:51:55 +0100

    Learning JQuery

--- JQuery/script.js
+++ JQuery/script.js
@@ -2,3 +2,9 @@ $("h1").css("color","red");
 $("h1").text("Great Day!");
 $("h2").text("Fun day too!");
 $("h2").addClass("funday");
+$("h1").click(function() {$("h1").css("color", "purple")});
+
+$("button").click(function(){$("h2").removeClass()});
+
+$("input").keypress(function(event){$("p").text([event.key]).css("color","white")});
+$("h1").on("mouseover", function(){$("h1".css("color","purple"))});
